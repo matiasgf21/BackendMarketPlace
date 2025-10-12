0ff1b0b7ecdd39c2587e9b5feb4d368c26283b24
matiasgf21 <matiasgf21@gmail.com>
2025-10-11 21:31:36 -0300

    productos.routes.js modificado

--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -8,23 +8,26 @@ const router = Router();
 router.get("/", async (_req, res) => {
   try {
     const { rows } = await pool.query(
-      "SELECT id, title AS nombre, price AS precio FROM products ORDER BY id"
+      "SELECT id, nombre AS title, precio AS price, image_url FROM products"
     );
-    res.status(200).json(rows);
+    res.json(rows);
   } catch (e) {
     console.error("GET /productos error:", e);
     res.status(500).json({ error: "DB error" });
   }
 });
 
-// GET /productos/:id
 router.get("/:id", async (req, res) => {
   try {
     const { rows } = await pool.query(
-      "SELECT id, title AS nombre, price AS precio FROM products WHERE id = $1",
+      "SELECT id, nombre AS title, precio AS price, image_url FROM products WHERE id = $1",
       [req.params.id]
     );
-    if (!rows[0]) return res.status(404).json({ message: "Producto no encontrado" });
+
+    if (!rows[0]) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
     res.json(rows[0]);
   } catch (e) {
     console.error("GET /productos/:id error:", e);
